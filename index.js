54fb4a3c73d948cd2bb85f0a37c3fdc2608e4178
Jeffrey Yoo <jeffyoo216@gmail.com>
2023-01-17 14:49:30 -0800

    Added ability to create directories

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const pdftk = require('node-pdftk');
 
 const { deleteTextFile, readTextFile, writeTextFile } = require('./lib/filesystem');
@@ -7,13 +8,30 @@ const { logError, log} = require('./util/log');
 
 async function main() {
   log('Starting pdf-splitter');
-  // TODO: Create output dir and temp dir
+  
+  const outputDir = './output';
+  const tmpDir = './output/tmp';
+
+  if (!fs.existsSync(outputDir)){
+    console.log('output dir does not exist, making it now');
+    fs.mkdirSync(outputDir);
+  } else {
+    console.log('output dir exists');
+  }
+
+  if (!fs.existsSync(tmpDir)){
+    console.log('tmp dir does not exist, making it now');
+    fs.mkdirSync(tmpDir);
+  } else {
+    console.log('tmp dir exists');
+  }
+
   // TODO: Look in files dir for all PDFs
 
   // 1. Break up megafile and create dump_data.txt
   await pdftk
     .input('./files/megafile.pdf')
-    .burst('./output/temp/%01d.pdf')
+    .burst('./output/tmp/%01d.pdf')
     .then(() => log('Finished breaking up megafile.pdf'))
     .catch(err => logError('Error while breaking up megafile, error:', err));
   
@@ -24,12 +42,12 @@ async function main() {
   const parsedTextData = textToJson(txtFile);
   
   // Uncomment to create output.json (for dev work)
-  // await writeTextFile(parsedTextData, './output/temp/output.json');
+  await writeTextFile(parsedTextData, './output/tmp/output.json');
 
   // 4. Iterate through parsedTextData and concatenate PDF files
   for (let i = 1; i < parsedTextData.length; i++) {
     const { HsmvCrashReportNum, totalNumPages, pages } = parsedTextData[i];
-    const formattedInputObj = inputObjFormatter(pages, './output/temp');
+    const formattedInputObj = inputObjFormatter(pages, './output/tmp');
     const formattedCatStr = Object.keys(formattedInputObj).join(' ');
     
     // TODO: Does there have to be an await here?
@@ -38,13 +56,13 @@ async function main() {
       .input(formattedInputObj)
       .cat(formattedCatStr)
       .output(`./output/${HsmvCrashReportNum}.pdf`)
-      .then(() => log(`Made PDF file: ${HsmvCrashReportNum}.pdf (${totalNumPages} pages)`))
-      .catch(err => logError('Error while concatenating PDF files, error:', err));
+      .then(() => log(`Successfully made PDF file: ${HsmvCrashReportNum}.pdf (${totalNumPages} pages)`))
+      .catch(err => logError(`Error while concatenating ${HsmvCrashReportNum}.pdf. Error: ${err}`));
   }
   
   log('Finished combining all PDFs. Files located in /output');
 
-  // TODO: Delete doc_data.txt and temp PDF files
+  // TODO: Delete doc_data.txt and tmp PDF files
   deleteTextFile('./doc_data.txt');
   log('End');
 }
